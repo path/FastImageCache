76602dd572288836816c581ec58a178208956cb7
Rui Peres <rui.peres@just-eat.com>
2015-06-17 10:16:23 +0100

    Added the exposed headers

--- FastImageCache/FastImageCache/FastImageCache.h
+++ FastImageCache/FastImageCache/FastImageCache.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char FastImageCacheVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FastImageCache/PublicHeader.h>
 
-
+#import <FastImageCache/FICImageCache.h>
+#import <FastImageCache/FICEntity.h>
